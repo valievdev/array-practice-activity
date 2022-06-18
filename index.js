40d354ac965b42f598ed0ba6879fbca2ad689ba4
azat valiev <azvaliev@gmail.com>
2022-06-18 12:14:10 -0500

    added solution

--- index.js
+++ index.js
@@ -14,24 +14,27 @@ const fruits = [
 // ^ DO NOT EDIT THE ABOVE ^
 
 // change this to store the filtered fruits as mentioned in README
-const filteredFruits = fruits;
+const filteredFruits = fruits.filter(fruit => fruit !== 'banana');
 
 // change this store all fruits uppercase using .map()
-const uppercaseFruits = fruits;
+const uppercaseFruits = fruits.map(fruit => fruit.toUpperCase());
 
 // change this to the output of array.reduce and count the number of bananas in array
-const bananaCount = 0;
+const bananaCount = fruits.reduce((bananaTotal, currentFruit) => {
+	if (currentFruit === 'banana') return bananaTotal + 1;
+	return bananaTotal;
+}, 0);
 
 // change this to the store output of find to find the first fruit that has a length of 4
-const fourFruit = 'apple';
+const fourFruit = fruits.find(fruit => fruit.length === 4);
 
 // change this to use findIndex to find the index of the fruit with a length of 10
-const tenFruit = 18;
+const tenFruit = fruits.findIndex(fruit => fruit.length === 10);
 
 /*
 Sort the list of fruits here
 */ 
-
+fruits.sort();
 
 
 /*
